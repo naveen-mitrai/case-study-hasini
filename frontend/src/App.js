12f7bc2347f2dd00264d7de3810cd617ad795a22
hasiniV19 <83814896+hasiniV19@users.noreply.github.com>
2025-02-03 01:04:35 +0530

    Added routing in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography} from "@mui/material";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import RegisterDrone from "./components/RegisterDrone";
+import MedicationPage from "./components/MedicationPage"; // Import MedicationPage for the loading medication feature
 
 function App() {
   return (
-    <Container>
-      <Typography variant="h4" align="center" sx={{ mt: 3 }}>
-        Drone Delivery System
-      </Typography>
-      <RegisterDrone />
-     <Dashboard/>
-    </Container>
+    <Router>
+      <Container>
+        <Typography variant="h4" align="center" sx={{ mt: 3 }}>
+          Drone Delivery System
+        </Typography>
+
+        <Routes>
+          
+          <Route path="/" element={<Dashboard />} /> 
+          <Route path="/registerDrone" element={<RegisterDrone />} /> 
+          <Route path="/medication" element={<MedicationPage />} /> 
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
